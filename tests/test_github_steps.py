2a90ec4ba5b2ab6dec7f5df2fbab37fabdcbf40b
Maks747 <valol93@mail.ru>
2025-06-17 20:23:23 +0300

    Обновил тест по 10 уроку.

--- tests/test_github_steps.py
+++ tests/test_github_steps.py
@@ -6,7 +6,7 @@ from model.issues_page import Issue_page
 
 @allure.tag("Github")
 @allure.severity(Severity.CRITICAL)
-@allure.label("Maksim")
+@allure.label("owner","Maksim")
 @allure.feature("Задачи в репозитории")
 @allure.story("Название репозитория содержит правильный заголовок")
 @allure.link("https://github.com", name="Testing")
@@ -17,3 +17,5 @@ def test_github_steps():
     issue.go_to_repo('Maks747/qa_guru_python_20_hw_10')
     issue.go_to_issue('Issue for HomeWork10')
     issue.assert_tittle_text('Issue for HomeWork10')
+def test_allure_labels():
+    pass
